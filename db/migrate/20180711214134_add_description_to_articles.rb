14ab10454882bd17052b44e7bca012c17a1a2917
Julie Fang <j47548081@gmail.com>
2018-07-12 22:14:34 +0000

    Create new template, action and route

--- db/migrate/20180711214134_add_description_to_articles.rb
+++ db/migrate/20180711214134_add_description_to_articles.rb
@@ -1,5 +1,6 @@
 class AddDescriptionToArticles < ActiveRecord::Migration
   def change
+    # add_column (Table name), (attr name), (data type)
     add_column :articles, :description, :text
     add_column :articles, :created_at, :datetime
     add_column :articles, :updated_at, :datetime
